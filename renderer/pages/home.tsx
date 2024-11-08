dcb1668f54a645807504024cb14f5a77551f289a
Kenneth1219 <kmanuel531@gmail.com>
2024-11-08 23:50:04 +0800

    right side

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,35 +1,44 @@
-import React from 'react'
-import Head from 'next/head'
-import Link from 'next/link'
-import Image from 'next/image'
+import React from "react";
+import Head from "next/head";
+import moment from "moment";
 
 export default function HomePage() {
+  const date = moment().utcOffset("+08:00").format("ddd, MMMM DD YYYY");
+  const time = moment().format("LT");
+
   return (
     <React.Fragment>
       <Head>
-        <title>Home - Nextron (with-tailwindcss)</title>
+        <title>CMI Canteen</title>
       </Head>
-      <div className="grid grid-col-1 text-2xl w-full text-center">
-        <div>
-          <Image
-            className="ml-auto mr-auto"
-            src="/images/logo.png"
-            alt="Logo image"
-            width={256}
-            height={256}
-          />
+      <main className=" flex flex-row gap-x-4 w-sceen h-screen bg-[#E4EFFF] p-4">
+        <div className=" w-[70%] bg-white rounded-xl h-full p-4"></div>
+
+        <div className=" w-[30%] h-full flex flex-col gap-y-4">
+          <div className=" w-full h-[30%] bg-white rounded-xl text-center flex flex-col gap-y-2 justify-center items-center">
+            <p className=" font-semibold text-3xl text-[#002147]">{date}</p>
+            <p className=" font-semibold text-2xl text-[#002147]">{time}</p>
+          </div>
+          <div className=" w-full h-[70%] bg-white rounded-xl p-4 pt-10">
+            <h1 className=" text-lg mb-4">Enter Amount: </h1>
+            <div className=" w-full">
+              <input
+                type="number"
+                className=" p-4 w-full border border-[#002147] rounded-lg outline-none"
+              />
+            </div>
+            <div className=" w-full grid grid-cols-2 mt-4 gap-4">
+              <button className=" rounded-xl bg-[#002147] p-4 text-white">20</button>
+              <button className=" rounded-xl bg-[#002147] p-4 text-white">50</button>
+              <button className=" rounded-xl bg-[#002147] p-4 text-white">100</button>
+              <button className=" rounded-xl bg-[#002147] p-4 text-white">200</button>
+              <button className=" rounded-xl bg-[#002147] p-4 text-white">500</button>
+              <button className=" rounded-xl bg-[#002147] p-4 text-white">1000</button>
+
+            </div>
+          </div>
         </div>
-        <span>⚡ Electron ⚡</span>
-        <span>+</span>
-        <span>Next.js</span>
-        <span>+</span>
-        <span>tailwindcss</span>
-        <span>=</span>
-        <span>💕 </span>
-      </div>
-      <div className="mt-1 w-full flex-wrap flex justify-center">
-        <Link href="/next">Go to next page</Link>
-      </div>
+      </main>
     </React.Fragment>
-  )
+  );
 }
